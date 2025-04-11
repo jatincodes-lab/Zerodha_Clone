2f16cce70a8e9ef133cf18dd0e02bbd7fe04cba9
jatincodes-lab <jraj786182@gmail.com>
2025-04-11 13:28:50 +0530

    Update Signup.jsx

--- frontend/src/landing-page/signup/Signup.jsx
+++ frontend/src/landing-page/signup/Signup.jsx
@@ -24,7 +24,7 @@ const Signup = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:8080/signup",
+        "https://zerodha-clone-aler.vercel.app/signup",
         formData,
         {
           withCredentials: true,
