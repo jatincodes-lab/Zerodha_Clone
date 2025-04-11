9017c1667a91dc9a68a63c5e2e6805b2a8e29273
jatincodes-lab <jraj786182@gmail.com>
2025-04-11 13:09:17 +0530

    add vercel.json

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 8080;
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173", // frontend origin
+    origin: "https://zerodha-clone-25ma.vercel.app", // frontend origin
     credentials: true, // allow credentials (cookies)
   })
 );
